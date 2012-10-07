c46b95ac21501bd4ebf6c5347a563f5b222d361f
Charles Blaxland <charles.blaxland@gmail.com>
2012-10-07 12:09:24 +1100

    Made a configurable welcome message #71

--- app/src/main/java/app/controller/HomeController.java
+++ app/src/main/java/app/controller/HomeController.java
@@ -1,5 +1,6 @@
 package app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+    @Value("${message}")
+    private String message;
 
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello World!");
+        model.addAttribute("message", message);
         return "home";
     }
 }
